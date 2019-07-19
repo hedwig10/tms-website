3c3d9ff23ead2e08a916bf3ba50efaa0455c8570
Ricardo Quintanar <lrq1012@gmail.com>
2019-07-19 13:52:21 -0500

    added components folder, created first components

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Card() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <p className="card-text">
+                    <h1>Making Logistics Easy</h1>
+                    <h4>TMS is a leader in transporting cargo that requires special handling such as oversized, heavy weight and high-risk products. We utilize state of the art electronic freight security measures to monitor and secure goods, and have years of experience delivering time sensitive cargo and heavy equipment in the toughest locations.
+</h4>
+<h2>Consulting Services</h2>
+<p>Finding the right partner and proper procedure to improve supply chain visibility, drive down costs and minimize liability can be challenging. Our bird's eye view of a supply chain allows us to identify risks and inefficiencies that could cripple organizations if left undetected. Our specialties include: re-engineering, start up and business process improvement.</p>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/components/tmshome.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HelloDiv() {
+  return (
+    <div>
+      <h1>Trade Management Solutions</h1>
+      <h2>Our Services</h2>
+      <ul>
+        <li>Air Transport</li>
+        <li>Ocean Transport</li>
+        <li>Over The Road Transport</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HelloDiv;
