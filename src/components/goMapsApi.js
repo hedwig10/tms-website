6ffbdc67628035f18d97ff9f246dbeb42ba93b1a
Ricardo Quintanar <lrq1012@gmail.com>
2019-07-26 10:46:34 -0500

    created security changes for website components

--- src/components/goMapsApi.js
+++ src/components/goMapsApi.js
@@ -13,7 +13,8 @@ export default function () {
 return( 
 <div style={{width: '50vw' ,height: '100vh'}}>
   <WrappedMap 
-    googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB6kkVpRxna2HJWKNLXR_R1-519OrtFCqg'}
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
+    libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
     loadingElement={<div style={{ height: `100%` }} />}
     containerElement={ <div style={{ height: `400px` }} />}
     mapElement= {<div style={{ height: `100%` }} />}
